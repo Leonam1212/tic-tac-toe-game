5f75ab926b6f387ee6778a55036d18827dd3cf03
Leonam Rodrigues <leonam_5000@hotmail.com>
2022-07-10 15:13:41 -0300

    Feature(components/board): added a board logic

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,20 +1,104 @@
-import { Cell, Board, Button, TurnMessage, WinnerMessage } from "./style";
+import { useEffect, useState } from "react";
+import { Cell, Board, Button, TurnMessage, ResultMessage } from "./style";
+
+type Players = "O" | "X";
 
 export const BoardComponent = () => {
+  const [playerXSelections, setPlayerXSelections] = useState<Number[]>([]);
+  const [playerOSelections, setPlayerOSelections] = useState<Number[]>([]);
+  let playerSelections: Number[] = [];
+  const [matches, setMatches] = useState(0);
+  console.log("matchees", matches);
+  // Turnos
+  const [turn, setTurn] = useState<Players>("X");
+
+  // Vencedor
+  const [winner, setWinner] = useState<Players | undefined>(undefined);
+
+  // Empate
+  const [draw, setDraw] = useState<boolean | undefined>(undefined);
+
   const squares = new Array(9).fill(undefined);
+
+  const [boardSquares, setBoardSquares] = useState(squares);
+
+  console.log(boardSquares);
+
+  const handleClick = (index: number) => {
+    if (winner) return undefined;
+    if (boardSquares[index] !== undefined) return null;
+
+    setBoardSquares(
+      boardSquares.map((item, itemIndex) => (itemIndex === index ? turn : item))
+    );
+
+    setTurn((element) => (element == "X" ? "O" : "X"));
+  };
+
+  const checkVictoryConditions = () => {
+    const victoryLines = [
+      [boardSquares[0], boardSquares[1], boardSquares[2]],
+      [boardSquares[3], boardSquares[4], boardSquares[5]],
+      [boardSquares[6], boardSquares[7], boardSquares[8]],
+
+      [boardSquares[0], boardSquares[4], boardSquares[8]],
+      [boardSquares[2], boardSquares[4], boardSquares[6]],
+      [boardSquares[0], boardSquares[3], boardSquares[6]],
+
+      [boardSquares[1], boardSquares[4], boardSquares[7]],
+      [boardSquares[2], boardSquares[5], boardSquares[8]],
+    ];
+
+    victoryLines.forEach((line) => {
+      if (line.every((line) => line === "X")) {
+        setWinner("X");
+      }
+      if (line.every((line) => line === "O")) {
+        setWinner("O");
+      }
+    });
+  };
+
+  const checkDraw = () => {
+    boardSquares.forEach((cell) => {
+      if (cell !== undefined) {
+        console.log("draw");
+      }
+    });
+  };
+
+  const reset = () => {
+    setWinner(undefined);
+    setDraw(undefined);
+    setTurn("X");
+    setBoardSquares(squares);
+  };
+
+  useEffect(checkVictoryConditions, [boardSquares]);
 
   return (
     <>
-      <WinnerMessage>X é o vitorioso</WinnerMessage>
-      <TurnMessage>X, ataque agora!</TurnMessage>
+      {winner && <ResultMessage>{winner} venceu</ResultMessage>}
+      {draw && <ResultMessage>Empate</ResultMessage>}
+      {!winner && <TurnMessage>{turn}, ataque agora!</TurnMessage>}
 
       <Board>
-        {squares.map((_, i) => (
-          <Cell>TIC</Cell>
+        {boardSquares.map((item, i) => (
+          <Cell key={i} onClick={() => handleClick(i)}>
+            {item}
+          </Cell>
         ))}
       </Board>
 
-      <Button>Reiniciar</Button>
+      {<Button onClick={reset}>Reiniciar</Button>}
     </>
   );
 };
+
+{
+  /* <ResultMessage>X é o vitorioso</ResultMessage>
+      <TurnMessage>X, ataque agora!</TurnMessage> */
+}
+{
+  /* <ResultMessage>Empate</ResultMessage> */
+}
--- src/components/board/style.tsx
+++ src/components/board/style.tsx
@@ -1,4 +1,10 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const spin = keyframes`
+    50% {
+    background-position: 100% 50%;
+  }
+`;
 
 export const Board = styled.div`
   width: 310px;
@@ -7,7 +13,7 @@ export const Board = styled.div`
   justify-content: center;
   flex-wrap: wrap;
 
-  gap: 0.6rem;
+  gap: 1rem;
 
   @media (min-width: 768px) {
     width: 500px;
@@ -18,13 +24,20 @@ export const Cell = styled.div`
   width: 80px;
   height: 80px;
 
-  background: var(--main-white);
+  background: #1c1b29;
   box-shadow: 4px 4px 10px rgba(0, 0, 0, 0.5);
+
+  color: var(--main-white);
+  font-size: 45px;
+  font-weight: bold;
+
+  --border-width: 5px;
+
+  position: relative;
 
   display: flex;
   justify-content: center;
   align-items: center;
-
   border-radius: 8px;
 
   cursor: pointer;
@@ -33,10 +46,35 @@ export const Cell = styled.div`
     width: 150px;
     height: 150px;
   }
+
+  :after {
+    border: 8px;
+    position: absolute;
+    content: "";
+    top: calc(-1 * var(--border-width));
+    left: calc(-1 * var(--border-width));
+    z-index: -1;
+    width: calc(100% + var(--border-width) * 2);
+    height: calc(100% + var(--border-width) * 2);
+    background: linear-gradient(
+      60deg,
+      #fd004c,
+      #fe9000,
+      #3edf4b,
+      #3363ff,
+      #b102b7,
+      #fff020,
+      #fd004c
+    );
+    background-size: 300% 300%;
+    background-position: 0 50%;
+    border-radius: calc(2 * var(--border-width));
+    animation: ${spin} 6s infinite linear;
+  }
 `;
 
 export const Button = styled.button`
-  background: var(--main-red);
+  background: var(--main-green);
   border-radius: 8px;
   border: none;
 
@@ -46,12 +84,12 @@ export const Button = styled.button`
   text-transform: uppercase;
 
   min-width: 250px;
-  width: 25%;
+  width: 32.5%;
   height: 50px;
 
   :hover {
     transition: 0.2s linear;
-    background: var(--main-green);
+    background: var(--main-red);
   }
 `;
 
@@ -62,7 +100,7 @@ export const TurnMessage = styled.p`
   font-weight: bold;
 `;
 
-export const WinnerMessage = styled.strong`
+export const ResultMessage = styled.strong`
   font-size: 2rem;
   color: var(--main-green);
   text-transform: uppercase;
